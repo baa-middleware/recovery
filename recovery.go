4820d79c4d2aeb551ea14f2e6c23592ba4e0e20b
coldstar <safeie@163.com>
2016-02-20 01:39:38 +0800

    add recovery middleware

---
+++ recovery.go
@@ -0,0 +1,25 @@
+// Package recovery provider a baa middleware which recovers from panics anywhere in the chain.
+package recovery
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/go-baa/baa"
+)
+
+// Recovery returns a baa middleware which recovers from panics anywhere in the chain
+// and handles the control to the centralized HTTPErrorHandler.
+func Recovery() baa.HandlerFunc {
+	return func(c *baa.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				trace := make([]byte, 1<<16)
+				n := runtime.Stack(trace, true)
+				c.Error(fmt.Errorf("panic recover\n %v\n stack trace %d bytes\n %s", err, n, trace[:n]))
+			}
+		}()
+
+		c.Next()
+	}
+}
